275fa6dd6db05b0de2073cddbaf45419137564b5
Hugo Molander <hugo.molander@gmail.com>
2022-01-25 17:03:14 +0100

    Completed assignment 2 "Comments:"

---
+++ P2Comments/Program.cs
@@ -0,0 +1,12 @@
+﻿//Author:Hugo Molander
+//1/25/2022
+//Hugo.molander@gmail.com
+
+class Program
+{
+    static void Main() // This is where the program starts "entry point"
+    {
+        //This writes the string within "" to the console as output
+        Console.WriteLine("Hello Woorld!!");
+    }
+}
