c64ead5a900dba943688c95a122ad59f38935454
Hugo Molander <hugo.molander@gmail.com>
2022-01-25 17:50:41 +0100

    Completed P6 Input assignment
    Tried to have some fun while making it.

---
+++ P6Input/Program.cs
@@ -0,0 +1,6 @@
+﻿//By Hugo Molander
+
+Console.WriteLine("Hello traveler! What might thy name be?");
+string name = Console.ReadLine();
+Console.WriteLine("Very well, even though it's ugly i shall call you " +name +"!");
+Console.ReadLine();
