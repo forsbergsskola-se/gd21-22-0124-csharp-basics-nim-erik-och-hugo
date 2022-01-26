d44960292e676b0d503cd7535aefce4431ae21ab
ErikBigMan <erik.mattsson@student.forsbergsskola.se>
2022-01-26 16:31:41 +0100

    P5 och p7

---
+++ P5Conversion/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = 5;
+string numberString = number.ToString();
+float numbersingel;
+
+Console.WriteLine(numberString);
+
+numberString = "56,3";
+numbersingel = Convert.ToSingle(numberString);
+number = Convert.ToInt32(numbersingel);
+Console.WriteLine(number);
---
+++ P7Operators/Program.cs
@@ -0,0 +1,8 @@
+﻿float number = (((5 + 7) * 3) -6) / 1.2F - 17;
+
+number = Convert.ToInt32(number);
+
+
+Console.WriteLine(number);
+
+
