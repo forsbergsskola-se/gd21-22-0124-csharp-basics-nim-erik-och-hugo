537059bc60a59d94ba3194ffa05577c7093fb845
Hugo Molander <hugo.molander@gmail.com>
2022-01-30 17:42:10 +0100

    trying to implement ai, working decently some bugs and unhandled exceptions and the player cant win at the moment

--- NimHM/Program.cs
+++ NimHM/Program.cs
@@ -1,56 +1,92 @@
-﻿string matches = "||||||||||||||||||||||||";
+﻿string matches = "||||||||||||||||||||||||";//scoreboard
 string removedMatches;
-string inputNumber;
+string inputNumber;//number given by player then converted to int "chosenNumber"
 int chosenNumber;
-int count = 24;
-
+int count = 24; // to keep check of remaining matches, removes from this every time an action is completed
+var randomGenerator = new Random(); //random number generator, later forced to remain within 1-4 which gives us numbers 1,2,3
+var numberOfTurns = 24;
 Console.WriteLine("Welcome to Nim!");
-Console.WriteLine("||||||||||||||||||||||||"+count);
 
 ThreadStart:
-if(count==0){Console.WriteLine("You win!");} 
-if(count<0){Console.WriteLine("you lose");}
-    
-    
-    Console.WriteLine("How many matches do you want to draw? (1,2,3)");
+if
+    (count == 0)
+{
+    Console.WriteLine("You lose!");
+
+}
+
+Console.WriteLine("How many matches do you want to remove? (1,2,3)");
+    Console.WriteLine(matches + count+" Remaining matches");
+
     inputNumber = Console.ReadLine();
     chosenNumber = int.Parse(inputNumber);
-    bool ChosenNumber1 = chosenNumber < 2 && chosenNumber > 0;
+    bool ChosenNumber1 = chosenNumber < 2 && chosenNumber > 0; // boolian values that gives player the correct action when inputting 1,2,3
     bool ChosenNumber2 = chosenNumber < 3 && chosenNumber > 1;
     bool ChosenNumber3 = chosenNumber < 4 && chosenNumber > 2;
 
+for (int i = 0; i < 100; i++)
     
+
+if ((i % 2) == 0)
+
+{
+    if (count > 0)
+    {
+        //ai script
+        if (count >3)
+        {
+            int randomInt = randomGenerator.Next(1, 4);
+            count = (count - randomInt);
+            removedMatches = matches.Remove(0, randomInt);
+            Console.WriteLine("ai  has removed " + randomInt + " match(es)");
+            matches = removedMatches;
+            
+        }
+        if (count>2)
+        {
+            
+        }
+        
+        
+    }
+
+
+}
+else //playerscript
+{
     if (ChosenNumber1)
     {
-        count=(count - 1);
+        numberOfTurns = (numberOfTurns - 1);
+        count = (count - 1);
         removedMatches = matches.Remove(0, 1);
-        Console.WriteLine(removedMatches+count);
-        matches = removedMatches;
+
         Console.WriteLine("You have removed " + chosenNumber + " match");
+        matches = removedMatches;
+        goto ThreadStart;
+    }
+
+    if (ChosenNumber2)
+    {
+        numberOfTurns = (numberOfTurns - 1);
+        count = (count - 2);
+        removedMatches = matches.Remove(0, 2);
+
+        Console.WriteLine("You have removed " + chosenNumber + " matches");
+        matches = removedMatches;
         goto ThreadStart;
 
     }
-    else
+
+    if (ChosenNumber3)
     {
-        if (ChosenNumber2)
-        {
-            count=(count - 2);
-            removedMatches = matches.Remove(0, 2);
-            Console.WriteLine(removedMatches+count);
-            matches = removedMatches;
-            Console.WriteLine("You have removed " + chosenNumber + " matches");
-            goto ThreadStart;
+        numberOfTurns = (numberOfTurns - 1);
+        count = (count - 3);
+        removedMatches = matches.Remove(0, 3);
+        matches = removedMatches;
+        Console.WriteLine("You have removed " + chosenNumber + " matches");
 
-        }
+        goto ThreadStart;
 
-        if (ChosenNumber3)
-        {
-            count=(count - 3);
-            removedMatches = matches.Remove(0, 3);
-            Console.WriteLine(removedMatches+count);
-            matches = removedMatches;
-            Console.WriteLine("You have removed " + chosenNumber + " matches");
-            goto ThreadStart;
-            
-        }
     }
+
+}
